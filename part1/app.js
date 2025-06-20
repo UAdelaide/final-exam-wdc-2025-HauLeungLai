5244afd36ad45b720635fa32d87c2d8806f887a8
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 09:27:01 +0000

    [automatic commit] 2025-06-20 09:27:01 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -71,7 +71,7 @@ async function main(){
                     res.json(rows);
             } catch (err) {
                 console.error(err);
-                res.status(500)
+                res.status(500).json({ error: 'Failed'})
             }
         })
     }
