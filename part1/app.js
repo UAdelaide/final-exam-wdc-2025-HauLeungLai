1518aa3e0f8ebae04681c91421335d22d7477596
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 09:45:55 +0000

    [automatic commit] 2025-06-20 09:45:55 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -46,7 +46,7 @@ async function main(){
 
         // /api/dogs
         // Return a list of all dogs with their size and owner's username
-        app.get('/api/dogs', async (req, res) =>{
+        app.get('/api/dogs', async (req, res) => {
             try {
                 const [rows] = await connection.execute(`
                     SELECT d.name AS dog_name, d.size, u.username AS owner_username
@@ -99,7 +99,7 @@ async function main(){
                 res.status(500).json({ error: 'Failed to fetch walkers summary.'});
             }
         });
-        app.listen(PORT, () =>{
+        app.listen(PORT, () => {
             console.log(`Server running at http://localhost:${PORT}`);
         });
     } catch (err) {
