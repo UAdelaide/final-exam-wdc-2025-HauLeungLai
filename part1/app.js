f6763734d4803bda4137215fbb53ef1a10559ece
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 09:26:00 +0000

    [automatic commit] 2025-06-20 09:26:00 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -65,8 +65,9 @@ async function main(){
                     SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
                     FROM walkrequests wr
                     JOIN dogs d ON wr.dog_id = d.dog_id
-                    JOIN users u ON wr.owner_id = u.user
-                    `)
+                    JOIN users u ON wr.owner_id = u.user_id
+                    WHERE wr.status = 'open'
+                    `);
             }
         })
     }
