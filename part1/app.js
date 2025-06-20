67dce2478cc0745c24b34617c916086ad81a5160
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:24:24 +0000

    [automatic commit] 2025-06-20 10:24:24 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,8 @@ async function main(){
         await connection.execute(`
             INSERT IGNORE INTO WalkRatings (request_id, rating, walker_id, owner_id) VALUES
             ((SELECT request_id FROM WalkRequests WHERE status = 'completed' AND
-            dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Charlie')), 5)
+            dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Charlie')), 5,
+            (SELECT user_id FROM ))
         `);
 
         // /api/dogs
