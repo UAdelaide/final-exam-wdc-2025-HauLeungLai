5c697a5a1cb2050439293b1a75446e583d373586
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 09:02:44 +0000

    [automatic commit] 2025-06-20 09:02:44 UTC 129.127.202.82

--- part1/app.js
+++ part1/app.js
@@ -40,7 +40,7 @@ async function main(){
             `)
         await connection.execute(`
             INSERT INTO rating (request_id, rating) VALUES
-            ((SELECT request_id FROM walkrequests WHERE)
+            ((SELECT request_id FROM walkrequests WHERE status = 'completed' AND dog)
         `)
     }
 }
